f479f9e646eb1b6e302fbbe922e5591a8cb1df94
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-06-27 16:24:30 -0300

    feat: ensure EmailAlreadyIsUserError is used

--- src/use-cases/create-user.js
+++ src/use-cases/create-user.js
@@ -2,6 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 import bcrypt from 'bcrypt';
 import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js';
 import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js';
+import { EmailAlreadyIsUserError } from '../errors/user.js';
 
 export class CreateUserCase {
     async execute(createUserParams) {
@@ -14,7 +15,7 @@ export class CreateUserCase {
             );
 
         if (userWithProviderEmail) {
-            throw new Error(`The provider e-mail is already in use.`);
+            throw new EmailAlreadyIsUserError(createUserParams.email);
         }
 
         const userId = uuidv4();
