f436f5ec6779dca610e76866355f4728eacd3fbb
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-07-07 15:09:48 -0300

    feat: implement get transactions by user ID endpoint and update transaction controller

--- src/repositories/postgres/transaction/get-transactions-by-user-id.js
+++ src/repositories/postgres/transaction/get-transactions-by-user-id.js
@@ -2,7 +2,7 @@ import { PostgresHelper } from '../../../db/postgres/helper.js';
 
 export class PostgresGetTransactionsByUserIdRepository {
     async execute(userId) {
-        const transactions = await PostgresHelper(
+        const transactions = await PostgresHelper.query(
             'SELECT * FROM transactions WHERE user_id = $1',
             [userId],
         );
