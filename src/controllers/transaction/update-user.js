d50e37897fdb788ecbe8d3add3469ef9ca47a4a7
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-07-08 15:18:39 -0300

    feat: add UpdateTransactionsController

---
+++ src/controllers/transaction/update-user.js
@@ -0,0 +1,68 @@
+import {
+    badRequest,
+    checkIfAmountIsValid,
+    checkIfIdIsValid,
+    checkIfTypeIsValid,
+    invalidAmoutResponse,
+    invalidIdResponse,
+    invalidTypeResponse,
+    ok,
+    serverError,
+} from '../helpers/index.js';
+
+export class UpdateTransactionsController {
+    constructor(updateTransactionsUseCase) {
+        this.updateTransactionsUseCase = updateTransactionsUseCase;
+    }
+    async execute(httpRequest) {
+        try {
+            const idIsValid = checkIfIdIsValid(
+                httpRequest.params.transactionId,
+            );
+
+            if (!idIsValid) {
+                return invalidIdResponse();
+            }
+
+            const params = httpRequest.body;
+
+            const allowedFields = ['name', 'date', 'amount', 'type'];
+
+            const someFieldsIsNotAllowed = Object.keys(params).some(
+                (field) => !allowedFields.includes(field),
+            );
+
+            if (someFieldsIsNotAllowed) {
+                return badRequest({
+                    message: 'Some provided field is not allowed',
+                });
+            }
+
+            if (params.amount) {
+                const amountIsvalid = checkIfAmountIsValid(params.amount);
+
+                if (!amountIsvalid) {
+                    return invalidAmoutResponse();
+                }
+            }
+
+            if (params.type) {
+                const typeIsValid = checkIfTypeIsValid(params.type);
+
+                if (!typeIsValid) {
+                    return invalidTypeResponse();
+                }
+            }
+
+            const transactions = await this.updateTransactionsUseCase.execute(
+                httpRequest.params.transactionId,
+            );
+
+            return ok(transactions);
+        } catch (error) {
+            console.error(error);
+
+            return serverError();
+        }
+    }
+}
