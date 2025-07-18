f99ab2ae66dee6ce754d64aa4e36cf5e19dfb9d6
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-07-18 14:54:34 -0300

    test: ensure return 400 if first_name is not provided

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -30,4 +30,26 @@ describe('Create User Controller', () => {
         expect(result.statusCode).toBe(201);
         expect(result.body).toBe(httpRequest.body);
     });
+
+    it('should reaturn 400 if first_name is not provided', async () => {
+        // Arrange
+        const createUserUseCase = new CreateUserUseCaseStub();
+        const createUserController = new CreateUserController(
+            createUserUseCase,
+        );
+
+        const httpRequest = {
+            body: {
+                last_name: 'Costa',
+                email: 'cleyton@gmail.com',
+                password: '123456',
+            },
+        };
+
+        // Act
+        const result = await createUserController.execute(httpRequest);
+
+        //  Assert
+        expect(result.statusCode).toBe(400);
+    });
 });
