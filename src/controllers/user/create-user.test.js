99a24ed3676d0403357428a4c0af014b3171b599
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-07-18 16:19:29 -0300

    test: ensure return 400 if CreateUserUseCase throws EmailAlreadyIsUserError

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -1,5 +1,6 @@
 import { faker } from '@faker-js/faker';
 import { CreateUserController } from './create-user';
+import { EmailAlreadyIsUserError } from '../../errors/user';
 
 describe('Create User Controller', () => {
     class CreateUserUseCaseStub {
@@ -232,5 +233,34 @@ describe('Create User Controller', () => {
 
         //  Assert
         expect(result.statusCode).toBe(500);
+    });
+
+    it('should return 400 if CreateUserUseCase throws EmailAlreadyIsUserError', async () => {
+        // Arrange
+        const createUserUseCase = new CreateUserUseCaseStub();
+        const createUserController = new CreateUserController(
+            createUserUseCase,
+        );
+
+        const httpRequest = {
+            body: {
+                first_name: faker.person.firstName(),
+                last_name: faker.person.lastName(),
+                email: faker.internet.email(),
+                password: faker.internet.password({
+                    length: 6,
+                }),
+            },
+        };
+
+        jest.spyOn(createUserUseCase, 'execute').mockImplementationOnce(() => {
+            throw new EmailAlreadyIsUserError(httpRequest.body.email);
+        });
+
+        // Act
+        const result = await createUserController.execute(httpRequest);
+
+        //  Assert
+        expect(result.statusCode).toBe(400);
     });
 });
