1e6f66ae4a154d622470b3e9e8d7242e120862f7
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-07-21 15:43:37 -0300

    test: ensure GetUserByIdController returns 404 if user is not found

--- src/controllers/user/get-user-by-id.test.js
+++ src/controllers/user/get-user-by-id.test.js
@@ -50,4 +50,18 @@ describe('GetUserByIdController', () => {
         //  Assert
         expect(result.statusCode).toBe(400);
     });
+
+    it('should return 404 if user is not found', async () => {
+        // Arrange
+        const { sut, getUserByIdUseCase } = makeSut();
+        jest.spyOn(getUserByIdUseCase, 'execute').mockResolvedValue(null);
+
+        // Act
+        const result = await sut.execute({
+            params: { userId: faker.string.uuid() },
+        });
+
+        //  Assert
+        expect(result.statusCode).toBe(404);
+    });
 });
