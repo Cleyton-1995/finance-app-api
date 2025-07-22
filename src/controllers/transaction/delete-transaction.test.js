97d98de89e96f95013de2bbd3dd5a939b3f4eed8
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-07-22 15:34:56 -0300

    test: ensure DeleteTransactionController returns 500 if DeleteTransactionUseCase throws

--- src/controllers/transaction/delete-transaction.test.js
+++ src/controllers/transaction/delete-transaction.test.js
@@ -69,4 +69,23 @@ describe('DeleteTransactionController', () => {
         //  Assert
         expect(result.statusCode).toBe(404);
     });
+
+    it('should return 500 if DeleteTransactionUseCase throws', async () => {
+        // Arrange
+        const { sut, deleteTransactionUseCase } = makeSut();
+
+        jest.spyOn(deleteTransactionUseCase, 'execute').mockRejectedValueOnce(
+            new Error(),
+        );
+
+        // Act
+        const result = await sut.execute({
+            params: {
+                transactionId: faker.string.uuid(),
+            },
+        });
+
+        //  Assert
+        expect(result.statusCode).toBe(500);
+    });
 });
