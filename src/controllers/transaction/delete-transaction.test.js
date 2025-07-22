a16ec58a510bb10b7eb239a4783e89b956148c13
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-07-22 15:27:02 -0300

    test: ensure DeleteTransactionController returns 400 when id is invalid

--- src/controllers/transaction/delete-transaction.test.js
+++ src/controllers/transaction/delete-transaction.test.js
@@ -35,4 +35,19 @@ describe('DeleteTransactionController', () => {
         //  Assert
         expect(response.statusCode).toBe(200);
     });
+
+    it('should return 400 when id is invalid', async () => {
+        // Arrange
+        const { sut } = makeSut();
+
+        // Act
+        const response = await sut.execute({
+            params: {
+                transactionId: 'invalid_id',
+            },
+        });
+
+        //  Assert
+        expect(response.statusCode).toBe(400);
+    });
 });
