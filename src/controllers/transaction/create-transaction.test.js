d4505693df37b7a39069abb3ae0a09d9316ac02f
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-07-23 15:00:01 -0300

    test: ensure CreateTransactionController returns call CreateTransactionUseCase with correct params

--- src/controllers/transaction/create-transaction.test.js
+++ src/controllers/transaction/create-transaction.test.js
@@ -215,4 +215,17 @@ describe('CreateTransactionController', () => {
         //  Assert
         expect(result.statusCode).toBe(500);
     });
+
+    it('should call CreateTransactionUseCase with correct params', async () => {
+        // Arrange
+        const { sut, createTransactionUseCase } = makeSut();
+
+        const executeSpy = jest.spyOn(createTransactionUseCase, 'execute');
+
+        // Act
+        await sut.execute(baseHttpRequest);
+
+        //  Assert
+        expect(executeSpy).toHaveBeenCalledWith(baseHttpRequest.body);
+    });
 });
