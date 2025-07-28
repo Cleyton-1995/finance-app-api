88df1421737383417c608b1ef8049228104580b6
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-07-28 14:38:46 -0300

    test: ensure DeleteUserUseCase call DeleteUserRepository with correct params

--- src/use-cases/user/delete-user.test.js
+++ src/use-cases/user/delete-user.test.js
@@ -38,4 +38,17 @@ describe('DeleteUserUseCase', () => {
         //  Assert
         expect(deleteUser).toEqual(user);
     });
+
+    it('should call DeleteUserRepository with correct params', async () => {
+        // Arrange
+        const { sut, deleteUserRepository } = makeSut();
+        const executeSpy = jest.spyOn(deleteUserRepository, 'execute');
+        const userId = faker.string.uuid();
+
+        // Act
+        await sut.execute(userId);
+
+        //  Assert
+        expect(executeSpy).toHaveBeenCalledWith(userId);
+    });
 });
