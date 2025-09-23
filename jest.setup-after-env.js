6641a82b7e9c12dff2a4253078697f8719f4c763
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-09-23 16:46:59 -0300

    refactor: optimize transaction deletion in jest setup using Prisma's transaction API

--- jest.setup-after-env.js
+++ jest.setup-after-env.js
@@ -1,6 +1,8 @@
 import { prisma } from './prisma/prisma';
 
 beforeEach(async () => {
-    await prisma.transaction.deleteMany({});
-    await prisma.user.deleteMany({});
+    await prisma.$transaction(async (tx) => {
+        await tx.transaction.deleteMany({});
+        await tx.user.deleteMany({});
+    });
 });
