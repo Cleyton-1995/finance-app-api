9342f0cf188b1e4586da9315c659b14508bba8f7
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-10-03 14:33:52 -0300

    refactor: streamline transaction update logic by creating a data object and handling date conversion conditionally

--- src/repositories/postgres/transaction/update-transaction.js
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -2,14 +2,22 @@ import { prisma } from '../../../../prisma/prisma.js';
 
 export class PostgresUpdateTransactionsRepository {
     async execute(transactionId, updateTransactionsParams) {
+        const data = { ...updateTransactionsParams };
+
+        if (
+            Object.prototype.hasOwnProperty.call(
+                updateTransactionsParams,
+                'date',
+            )
+        ) {
+            data.date = new Date(updateTransactionsParams.date);
+        }
+
         return await prisma.transaction.update({
             where: {
                 id: transactionId,
             },
-            data: {
-                ...updateTransactionsParams,
-                date: new Date(updateTransactionsParams.date),
-            },
+            data,
         });
     }
 }
