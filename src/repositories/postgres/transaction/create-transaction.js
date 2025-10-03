65c7e2d3562cbceb1243a990a1712452a860d6b0
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-10-03 15:09:53 -0300

    feat: enhance transaction creation by ensuring amount is stored as a decimal with two decimal places

--- src/repositories/postgres/transaction/create-transaction.js
+++ src/repositories/postgres/transaction/create-transaction.js
@@ -1,10 +1,14 @@
 import { prisma } from '../../../../prisma/prisma.js';
+import { Prisma } from '@prisma/client';
 
 export class PostgresCreateTransactionRepository {
     async execute(createTransactionParams) {
         return await prisma.transaction.create({
             data: {
                 ...createTransactionParams,
+                amount: new Prisma.Decimal(
+                    Number(createTransactionParams.amount).toFixed(2),
+                ),
                 date: new Date(createTransactionParams.date),
             },
         });
