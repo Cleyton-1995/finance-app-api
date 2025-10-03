a981728e422f4a105e794062ca5d9b818ee529e4
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-10-03 14:48:34 -0300

    feat: ensure PostgresUpdateTransactionsRepository throws TransactionNotFoundError if transaction is not found

--- src/repositories/postgres/transaction/update-transaction.js
+++ src/repositories/postgres/transaction/update-transaction.js
@@ -1,23 +1,33 @@
 import { prisma } from '../../../../prisma/prisma.js';
+import { TransactionNotFoundError } from '../../../errors/transaction.js';
 
 export class PostgresUpdateTransactionsRepository {
     async execute(transactionId, updateTransactionsParams) {
-        const data = { ...updateTransactionsParams };
+        try {
+            const data = { ...updateTransactionsParams };
 
-        if (
-            Object.prototype.hasOwnProperty.call(
-                updateTransactionsParams,
-                'date',
-            )
-        ) {
-            data.date = new Date(updateTransactionsParams.date);
+            if (
+                Object.prototype.hasOwnProperty.call(
+                    updateTransactionsParams,
+                    'date',
+                )
+            ) {
+                data.date = new Date(updateTransactionsParams.date);
+            }
+
+            return await prisma.transaction.update({
+                where: {
+                    id: transactionId,
+                },
+                data,
+            });
+        } catch (error) {
+            // Map Prisma's "record not found for update" to a domain error
+            if (error && error.code === 'P2025') {
+                throw new TransactionNotFoundError(transactionId);
+            }
+
+            throw error;
         }
-
-        return await prisma.transaction.update({
-            where: {
-                id: transactionId,
-            },
-            data,
-        });
     }
 }
