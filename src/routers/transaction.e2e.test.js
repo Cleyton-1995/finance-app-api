d153915e2522ce6790e57347e306f85964e87555
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-10-03 15:17:46 -0300

    test: ensure GET /api/transactions/:userId should return 404 when fetching transaction from a non-exists user

--- src/routers/transaction.e2e.test.js
+++ src/routers/transaction.e2e.test.js
@@ -102,4 +102,12 @@ describe('Transaction Routes E2E Tests', () => {
 
         expect(response.status).toBe(404);
     });
+
+    it('GET /api/transactions/:userId should return 404 when fetching transaction from a non-exists user', async () => {
+        const response = await request(app).get(
+            `/api/transactions?userId=${transaction.user_id}`,
+        );
+
+        expect(response.status).toBe(404);
+    });
 });
