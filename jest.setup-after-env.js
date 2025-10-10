4bff62266770f3d592ad791a51682a3993dcc7aa
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-10-10 15:31:10 -0300

    chore: uncomment database setup and cleanup logic in Jest configuration

--- jest.setup-after-env.js
+++ jest.setup-after-env.js
@@ -1,11 +1,11 @@
-// import { prisma } from './prisma/prisma';
+import { prisma } from './prisma/prisma';
 
-// // Ensure DATABASE_URL is set for tests
-// process.env.DATABASE_URL = 'postgresql://root:csc@localhost:5433/finance-app';
+// Ensure DATABASE_URL is set for tests
+process.env.DATABASE_URL = 'postgresql://root:csc@localhost:5433/finance-app';
 
-// beforeEach(async () => {
-//     await prisma.$transaction(async (tx) => {
-//         await tx.transaction.deleteMany({});
-//         await tx.user.deleteMany({});
-//     });
-// });
+beforeEach(async () => {
+    await prisma.$transaction(async (tx) => {
+        await tx.transaction.deleteMany({});
+        await tx.user.deleteMany({});
+    });
+});
