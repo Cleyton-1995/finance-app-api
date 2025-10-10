bbade23c5a18ba70a199f867589e1ff133992908
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-10-10 15:11:08 -0300

    chore: set DATABASE_URL for test environment in Jest setup files

--- jest.setup-after-env.js
+++ jest.setup-after-env.js
@@ -1,5 +1,8 @@
 import { prisma } from './prisma/prisma';
 
+// Ensure DATABASE_URL is set for tests
+process.env.DATABASE_URL = 'postgresql://root:csc@localhost:5433/finance-app';
+
 beforeEach(async () => {
     await prisma.$transaction(async (tx) => {
         await tx.transaction.deleteMany({});
