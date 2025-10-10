e1a4d1319ad2df0ed87cd1c19ef13bab67c7d19a
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2025-10-10 15:27:02 -0300

    ci: update Jest setup to comment out DATABASE_URL and create .env.test file in workflow

--- jest.global-setup.js
+++ jest.global-setup.js
@@ -3,9 +3,9 @@ import { execSync } from 'child_process';
 async function init() {
     execSync('docker compose up -d --wait postgres-test');
 
-    // Set DATABASE_URL for the test environment
-    process.env.DATABASE_URL =
-        'postgresql://root:csc@localhost:5433/finance-app';
+    // // Set DATABASE_URL for the test environment
+    // process.env.DATABASE_URL =
+    //     'postgresql://root:csc@localhost:5433/finance-app';
 
     execSync('npx prisma db push');
 }
